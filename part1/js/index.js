2f63c8312be1976e734ae27b2e0da49358032865
zenofjr <zfrancajunior@gmail.com>
2019-08-29 22:35:53 -0300

    update project

---
+++ part1/js/index.js
@@ -0,0 +1,10 @@
+
+
+function executar() {
+
+    const fill0 = tf.fill([1], 0);
+    const fill1 = tf.fill([2,2], 1);
+
+    document.getElementById("result").innerHTML = fill1.toString();   
+}
+ 
