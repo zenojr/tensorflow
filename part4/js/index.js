02c33be30c7d38f9923100f8641ef0e43f4d550c
zenofjr <zfrancajunior@gmail.com>
2019-11-10 20:42:32 -0300

    update project

--- part4/js/index.js
+++ part4/js/index.js
@@ -10,10 +10,20 @@ function executar() {
     // const concat = tensor2.concat(tensor3);
     const concat = tensor2.concat([ tensor2, tensor3 ]); // better
 
+    const tensor4 = tf.tensor([5, 6]);
+    const tensor5 = tf.tensor([7, 8 ]);
+    const stack = tf.stack([ tensor4, tensor5 ]);
+
+    const tensor6 = tf.tensor([1,2,3,4], [2,2]);
+    const reverse = tf.reverse(tensor6);
+
+    const tensor7 = tf.tensor([1,2,3,4,5,6,7,8], [2,4]);
+    const [tensor8, tensor9] = tf.split(tensor7, 2, 1);
+
     saida += 'antes: \n';
-    saida += tensor2.toString() + tensor3.toString() + '\n';
+    saida += tensor7.toString() + '\n';
     saida += 'depois' + '\n';
-    saida += concat.toString() + '\n';
+    saida += tensor8.toString() + tensor9.toString() + '\n';
     document.getElementById("result").innerHTML = saida.toString();   
 
     
